64723d63853ade0d58c4c99a8503fd3ecc9b1bf9
chuangfeng.wang <chuangfeng.wang@qunar.com>
2025-04-20 16:21:30 +0800

    commit

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -3,10 +3,33 @@
 	This problem requires you to implement a sorting algorithm
 	you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
-// I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T>(array: &mut [T])
+where
+    T: PartialOrd + Copy,
+{
+    let len = array.len();
+    // 如果数组长度小于等于1，无需排序
+    if len <= 1 {
+        return;
+    }
+
+    for i in 0..len {
+        // 标志位，检查是否发生交换
+        let mut swapped = false;
+        // 每次遍历后，最大的元素会冒泡到最后，所以可以减少内层循环次数
+        for j in 0..len - i - 1 {
+            if array[j] > array[j + 1] {
+                array.swap(j, j + 1);
+                swapped = true;
+            }
+        }
+
+        // 如果这一轮没有发生交换，说明数组已经有序
+        if !swapped {
+            break;
+        }
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -18,13 +41,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
